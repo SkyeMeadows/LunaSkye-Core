26cfecbb507d81def8e94e5b91a1d9acdf099a82
SkyeMeadows <skyemeadows20@gmail.com>
2025-12-12 23:45:33 -0600

    new take on command

--- modules/discord/MarketHand.py
+++ modules/discord/MarketHand.py
@@ -137,15 +137,10 @@ async def get_graph(
         item_id = name_to_id[item_key]
         safe_item_name = user_input_name.strip().replace(" ", "_").replace("/", "_")
         
-        command = [
-            sys.executable,
-            str(GRAPH_GENERATOR),
-            "--type_id", str(item_id),
-            "--market", str(market),
-        ]
+        command = [f"{sys.executable} {str(GRAPH_GENERATOR)} --type_id {str(item_id)} --market {str(market)}"]
 
         if days_history:
-            command.append(f"--days {str(days_history)}")
+            command.append(f" --days {str(days_history)}")
 
         log.debug(f"Running subprocess: {' '.join(command)}")
 
