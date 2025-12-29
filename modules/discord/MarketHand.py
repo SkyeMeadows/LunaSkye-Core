606fe38e0f38b4520a5477aacc09493484a17167
Skye <skyemeadows20@gmail.com>
2025-12-28 19:47:01 -0600

    attempting type fix

--- modules/discord/MarketHand.py
+++ modules/discord/MarketHand.py
@@ -238,6 +238,7 @@ async def item_summary(
         ]
 
         if days_history:
+            log.debug(f"Appending days history argument with days: {days_history}")
             command.append("--days")
             command.append(float(days_history))
 
