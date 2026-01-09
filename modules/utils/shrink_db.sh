4d2414894fbfd718869ffe25eaebae2f522d1628
--global <--global>
2026-01-08 18:09:55 -0600

    Updated DB management scripts

--- modules/utils/shrink_db.sh
+++ modules/utils/shrink_db.sh
@@ -70,32 +70,4 @@ echo "Integrity check on gsf compacted DB: OK" || {
     exit 1
 }
 
-# Rename originals to _OLD
-mv ./data/jita_market_prices.db ./data/jita_market_prices_OLD.db && \
-mv ./data/gsf_market_prices.db ./data/gsf_market_prices_OLD.db && \
-echo "Original DBs renamed to _OLD successfully." || {
-    echo "Error renaming original DBs: $?"
-    exit 1
-}
-
-# Rename compacted to originals
-mv ./data/jita_compacted.db ./data/jita_market_prices.db && \
-mv ./data/gsf_compacted.db ./data/gsf_market_prices.db && \
-echo "Compacted DBs renamed to originals successfully." || {
-    echo "Error renaming compacted DBs: $?"
-    exit 1
-}
-
-# Delete the _OLD databases
-rm ./data/jita_market_prices_OLD.db && \
-rm ./data/gsf_market_prices_OLD.db && \
-echo "OLD DBs removed successfully." || {
-    echo "Error removing OLD DBs: $?"
-    exit 1
-}
-
-# Delete the clones
-rm ./data/jita_market_prices_clone.db
-rm ./data/gsf_market_prices_clone.db
-
 echo "Script completed successfully."
